8efc4da9882d80ab85cc3f0db6caa6b0b0bbe097
Jonathan Voss <k98kurz@gmail.com>
2025-02-22 04:07:25 -0500

    slightly improved HMACAuthPlugin

--- netaio/auth.py
+++ netaio/auth.py
@@ -35,16 +35,23 @@ class HMACAuthPlugin:
     secret: bytes
 
     def __init__(self, config: dict):
+        """Initialize the HMAC auth plugin with a config. The config
+            must contain {"secret": <str|bytes>}.
+        """
         secret = config["secret"]
         if isinstance(secret, str):
             secret = secret.encode()
         self.secret = sha256(secret).digest()
 
     def make(self, auth_fields: AuthFieldsProtocol, body: BodyProtocol) -> None:
+        """If the nonce and ts fields are not present, generate them.
+            Then, create an hmac of the nonce, ts, and body and store it
+            in the hmac field.
+        """
         nonce = auth_fields.fields.get("nonce", b'')
         if len(nonce) != IV_SIZE:
             nonce = urandom(IV_SIZE)
-        ts = int(time())
+        ts = auth_fields.fields.get("ts", int(time()))
         auth_fields.fields.update({
             "nonce": nonce,
             "ts": ts,
@@ -52,14 +59,18 @@ class HMACAuthPlugin:
         })
 
     def check(self, auth_fields: AuthFieldsProtocol, body: BodyProtocol) -> bool:
+        """Check if the auth fields are valid for the given body."""
         ts = auth_fields.fields.get("ts", 0)
-        if ts == 0:
+        nonce = auth_fields.fields.get("nonce", None)
+        mac = auth_fields.fields.get("hmac", None)
+        if ts == 0 or nonce is None or mac is None:
             return False
         return check_hmac(
             self.secret,
-            auth_fields.fields["nonce"] + ts.to_bytes(4, "big") + body.encode(),
-            auth_fields.fields["hmac"]
+            nonce + ts.to_bytes(4, "big") + body.encode(),
+            mac
         )
 
     def error(self) -> MessageProtocol:
+        """Make an auth error message."""
         return make_error_response("HMAC auth failed")
